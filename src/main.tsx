ec0c3acf46cf6af179d1e16a59945f3affb9d7aa
Walid-sd <waliderraji95@gmail.com>
2026-09-07 15:16:07 +0100

    Wire game audio feedback into app

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import App from './App'
+import { installAudioBridge } from './audio-bridge'
+
+installAudioBridge()
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
