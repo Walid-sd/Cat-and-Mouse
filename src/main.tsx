c563b49759ed1ae0557fe70cab69e2ebf8b0f6f5
Walid-sd <waliderraji95@gmail.com>
2026-09-07 20:18:05 +0100

    Load progression and coin mobile styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
+import './progression.css'
 import App from './App'
 
 if (import.meta.env.PROD && 'serviceWorker' in navigator) {
